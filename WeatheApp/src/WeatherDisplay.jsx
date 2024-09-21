5b6cc935648dcfb6e7091413ca59e97448001e38
kwrona05 <kwrona05@gmail.com>
2024-09-21 12:59:28 +0200

    Create WeatherDisplay component

--- WeatheApp/src/WeatherDisplay.jsx
+++ WeatheApp/src/WeatherDisplay.jsx
@@ -1,4 +1,13 @@
-function WeatherDisplay() {
-  return;
+function WeatherDisplay({ weather }) {
+  const { name, main, weather: weatherDetails } = weather;
+
+  return (
+    <div>
+      <h2>City: {name}</h2>
+      <p>Temperature: {main.temp}°C</p>
+      <p>Humidity: {main.humidity}%</p>
+      <p>Details: {weatherDetails[0].description}</p>
+    </div>
+  );
 }
 export default WeatherDisplay;
