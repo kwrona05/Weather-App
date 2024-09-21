401dd0513475ecfc277399f3a7e95f62383b6a83
kwrona05 <kwrona05@gmail.com>
2024-09-21 12:52:33 +0200

    create SearchBar component

--- WeatheApp/src/SearchBar.jsx
+++ WeatheApp/src/SearchBar.jsx
@@ -1,4 +1,26 @@
-function SearchBar() {
-  return;
+import { useState } from "react";
+
+function SearchBar({ onCityChange }) {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue) {
+      onCityChange(inputValue);
+      setInputValue("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter your city"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button type="submit">Show weather</button>
+    </form>
+  );
 }
 export default SearchBar;
